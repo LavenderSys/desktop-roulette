0be24fd04bea422d1202f2d84cb1da843ebc7b6f
The Lavender System <81138994+LavenderSys@users.noreply.github.com>
2025-02-07 22:18:09 -0800

    Add files via upload

---
+++ DESKTOP-ROULETTE.py
@@ -0,0 +1,14 @@
+import random
+import os
+
+username = os.getlogin()
+
+folder_path = ("C:/Users/"+username+"/Desktop")
+
+desktop = os.listdir(folder_path)
+desktop.remove("desktop.ini")    
+
+random_variable = random.choice(desktop)
+
+print("Launching",random_variable+"...")
+os.startfile("C:/Users/"+username+"/Desktop/"+random_variable)
